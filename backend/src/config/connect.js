bec52a9bee90f2ea6170cf5c904ff63f4162a2e1
Drzaln <drzaln@null.net>
2019-07-21 07:42:21 +0700

    masih di pinjam

--- backend/src/config/connect.js
+++ backend/src/config/connect.js
@@ -2,15 +2,15 @@ require('dotenv').config()
 const sql = require('mysql')
 
 const connect = sql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASS,
+    // database: process.env.DB_NAME,
 
-    // host: "remotemysql.com",
-    // user: "NsS3iZedQb",
-    // password: "ahOgeEQ2oy",
-    // database: "NsS3iZedQb",
+    host: "remotemysql.com",
+    user: "NsS3iZedQb",
+    password: "ahOgeEQ2oy",
+    database: "NsS3iZedQb",
 })
 
 connect.connect((err) => {
